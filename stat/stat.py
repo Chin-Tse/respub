10aa730170560b6c928083391e0a77d8a9dd2b71
chin-tse <zhoujinze@gmail.com>
2014-09-27 02:43:29 +0800

    processing...

--- stat/stat.py
+++ stat/stat.py
@@ -3,15 +3,17 @@ import numpy as np
 import configobj
 import time
 import threading
-import IPy
 
 import sys
 import types
 
-from IPy import IP, IPSet
+import IPy
+import stat_cfg
 
 from pandas import Series, DataFrame
 from configobj import ConfigObj
+from stat_cfg import auto_cfg, spc_gcfg, acfg_entry
+from IPy import IP, IPSet
 
 if sys.version_info >= (3,):
     INT_TYPES = (int,)
@@ -21,27 +23,7 @@ else:
     INT_TYPES = (int, long)
     STR_TYPES = (str, unicode)
 
-import stat_cfg
-from stat_cfg import auto_gcfg, spc_gcfg
-
-def stat_filter(data, cfg_fmt, dmlist, match):
-  dlen = len(dmlist)
-  mlen = len(match)
-  if dlen != mlen:
-    raise ValueError("Config Error:", dmlist, match)
-
-  cp_list = (cfg_fmt['srcip'], cfg_fmt['dstip'])
-  print cp_list
-  for dm, mt in zip(dmlist, match):
-    if dm in cp_list:
-      dtmp = data.copy()
-
-    #dtmp = data[data[dm] == mt]
-   
-
-def stat_grp(data, colist, olist):
-  pass
-
+# stat parameters
 class stat_arg(object):
   def __init__(self):
     self.pline = 0;
@@ -53,7 +35,79 @@ class stat_arg(object):
     self.logfile = self.config['path']['input']
     print self.logfile
     self.scfg = spc_gcfg(self.cfg_sgp, self.cfg_fmt)
+    self.max_col = 0
+    self.max_col = max(int(col) for col in self.cfg_fmt.values()) 
+    self.max_col += 1
+    self.cfg_fmt['src_net'] = self.max_col
+    self.max_col += 1
+    self.cfg_fmt['dst_net'] = self.max_col
+ 
+# process subnet thing
+def stat_prepare(data, args, spc_idx):
+  # conver with user cfg
+  args.scfg.set_spc_matchlist(spc_idx, args.scfg.get_spc_dmlist(spc_idx))
+  dmlist = args.scfg.get_spc_matchlist(spc_idx)
+  match = args.scfg.get_spc_dmmatch(spc_idx)
+  cfg_fmt = args.cfg_fmt
 
+  dlen = len(dmlist)
+  mlen = len(match)
+  if dlen != mlen:
+    raise ValueError("Config Error:", dmlist, match)
+
+  if len(data) < 1:
+    print "No new data for process!"
+    return None
+
+  cmpstr = "True"
+  for idx, dm, mt in zip(range(dlen), dmlist, match):
+    print '--------', dmlist[idx], cfg_fmt['srcip'], cfg_fmt['dstip']
+    if dm == int(cfg_fmt['srcip']):
+      dmlist[idx] = int(cfg_fmt['src_net'])
+      try:
+        prefix = IP(mt).prefixlen()
+        data[dmlist[idx]] = data[dm].map(lambda x: str(IP(x).make_net(prefix)))
+        mt = str(IP(mt))
+        dm = dmlist[idx]
+      except:
+        raise ValueError("Error srcip config:", mt)
+      
+    elif dm == int(cfg_fmt['dstip']):
+      dmlist[idx] = int(cfg_fmt['dst_net'])
+      try:
+        prefix = IP(mt).prefixlen()
+        data[dmlist[idx]] = data[dm].map(lambda x: str(IP(x).make_net(prefix)))
+        mt = str(IP(mt))
+        dm = dmlist[idx]
+      except:
+        raise ValueError("Error srcip config:", mt)
+
+    # origanize cmp string
+    # target cmp string should be like 
+    if isinstance(data.iat[0, dm], STR_TYPES):
+      mtstr = "'" + str(mt) + "'" 
+    else:
+      mtstr = str(mt)
+
+    strtmp = " & (" + "data[" + str(dm) + "] == " + mtstr + ")" 
+    cmpstr = cmpstr + strtmp
+
+  # filter out target data
+  print cmpstr
+  return data[eval(cmpstr)]
+  
+def stat_grp(df, agcfg_item):
+  for dm in colist:
+    gp = df.groupby(dm)
+    for gkey, mgrp in gp:
+      val_list = list(gp.sum().loc(gkey, olist))
+      dbgstr = str(gkey) + str(val_list)
+    
+  pass
+
+def stat_print(data, olist, thd_list):
+  pass
+   
 def stat_tm_action(args=[]):
   print 'timeout:'
   #arg = args[0]
@@ -62,12 +116,24 @@ def stat_tm_action(args=[]):
   t.start()
 
   # read data
-  data = pd.read_csv(args.logfile, header=None, parse_dates=[0], index_col=0, skiprows=args.pline)
-  print len(data)
+  #data = pd.read_csv(args.logfile, header=None, nrows=100, parse_dates=[0], skiprows=args.pline)
+  print args.logfile
+  data = pd.read_csv(args.logfile, header=None, nrows=100, parse_dates=[0], skiprows=args.pline)
+  
 
-  spc_dmlist = args.scfg.get_spc_dmlist(0)
-  spc_dmmatch = args.scfg.get_spc_dmmatch(0)
-  stat_filter(data, args.cfg_fmt, spc_dmlist, spc_dmmatch)
+  slen = args.scfg.get_spcfg_num()
+  print "slen:", slen 
+  for sidx in range(slen):
+    dtmp = stat_prepare(data, args, sidx)
+    for aidx in range(args.scfg.get_spcagc_num(sidx)):
+      agc_item = agc_entry()
+      if args.scfg.get_spcagc_item(sidx, aidx, agc_item) != True:
+        print "Error: Get spcagc item fail!"
+        continue
+      
+      print "start restructure data:", sidx, aidx, ag_dmlist, out_dmlist
+      dtmp = stat_grp(dtmp, agc_item)
+      pass
 
   # genrate dst data
 
--- stat/stat_cfg.py
+++ stat/stat_cfg.py
@@ -6,19 +6,15 @@
 # @version 0.0.1
 # @date 2014-09-24
 
-import pandas as pd
-import numpy as np
 import configobj
 import time
-import threading
 import IPy
+import copy
 
 import sys
 import types
 
 from IPy import IP, IPSet
-
-from pandas import Series, DataFrame
 from configobj import ConfigObj
 
 if sys.version_info >= (3,):
@@ -34,52 +30,18 @@ global config
 cfgfile = './config.txt'
 config = ConfigObj(cfgfile)
 
-def tm_action(args=[]):
-    print 'timeout:'
-    print args
-    global t        #Notice: use global variable!
-    t=threading.Timer(2, tm_action, [pline])
-    t.start()
-    # do data process
-
-file='/home/see/src/respub/stat/demo4.log'
-pline=3
-
-data3=pd.read_csv(file, header=None, parse_dates=[0], index_col=0, skiprows=pline)
-#srcip = IP(data3[2]+'/'+'24').net
-dtmp = data3
-dtmp[2] = dtmp[2].map(lambda x : str(IP(x).make_net(24)))
-print dtmp[2]
-
-
-class auto_gcfg(object):
-  def __init__(self, section, ksec):
-    self.acfg = section
-    self.alen = len(section)
-    self.acfg_name = []
-    self.acfg_kidx = []
-    self.acfg_key = []
-    for icfg, acfg_item in enumerate(self.acfg):
-      #cfg-name
-      self.acfg_name.append(acfg_item)
-      self.acfg_kidx.append([])
-      self.acfg_key.append([])
-      #self.acfg[icfg][0] = acfg_item
-      for ikey, key in enumerate(self.acfg[acfg_item]):
-        try:
-          print ikey, key
-          self.acfg_key[icfg].append(key)
-          self.acfg_kidx[icfg].append(int(ksec[key]))
-          pass
-        except KeyError:
-          raise ValueError("%r, Invalid config key! %r", key, ksec.keys())
-
-    print self.acfg_name
-    print self.acfg_kidx
-    print self.acfg_key
-    
-    pass
-
+# convert like "abc, cde, dcc, 10" -> ['abc', 'cde', 'ddc', 10]
+def string2list(lstr):
+  if (len(lstr) == 0):
+    lstr = "[]"
+  else:
+    lstr = lstr[:-1] + lstr[-1] + '\']'
+    #lstr = lstr.replace(lstr[-1], lstr[-1] + '\']', 1)
+    lstr = lstr.replace(lstr[0],  '[\'' + lstr[0], 1)
+    lstr = lstr.replace(",", '\', \'')
+    #print '--------------', lstr
+  lst = list(eval(lstr))
+  return lst
 
 def parse_agpstr(string):
   #convert to str
@@ -95,35 +57,126 @@ def parse_agpstr(string):
   if index < 0:
     raise ValueError("Config do not have a right format!", str(string))
   acfg_dm = agpstr[:index]
-  if (len(acfg_dm) == 0):
-    acfg_dm="()"
-  else:
-    acfg_dm = acfg_dm.replace(acfg_dm[ - 1], acfg_dm[ - 1] + '\')', 1)
-    acfg_dm = acfg_dm.replace(acfg_dm[0],  '(\'' + acfg_dm[0], 1)
-    acfg_dm = acfg_dm.replace(",", '\', \'')
-    #print '--------------', acfg_dm
-  acfg_key = list(eval(acfg_dm))
+  acfg_key = string2list(acfg_dm)
 
   #find value domain
   agpstr = agpstr[index + 1:]
   index = agpstr.find('|')
   if index < 0:
     raise ValueError("Config do not have a right format!", str(string))
-  acfg_value = agpstr[:index]
-  acfg_value = acfg_value.replace(" ", "")
-  #print "acfg_value", acfg_value
+  acfg_colname = agpstr[:index]
+  acfg_colname = acfg_colname.replace(" ", "")
+  acfg_stat_colname = string2list(acfg_colname)
 
   #find threshold
-  acfg_thd = agpstr[index+1:]
-  if acfg_thd.isdigit() != True:
-    raise ValueError("Config do not have a right format!", str(string))
-  else:
-    #print 'success'
-    pass
+  acfg_thdstr = agpstr[index+1:]
+  acfg_thds = string2list(acfg_thdstr)
+  for idx, thd in enumerate(acfg_thds):
+    if thd.isdigit() != True:
+      raise ValueError("Config do not have a right format!", str(string), thd)
+    acfg_thds[idx] = int(thd)
 
-  return acfg_key, acfg_value, acfg_thd
+  return acfg_key, acfg_stat_colname, acfg_thds
 
+##### Maybe there should had a class define every acfg_item ####
+class acfg_entry():
+  def __init__(self):
+    self.acfg_name = ""
+    self.acfg_dmlist = []
+    self.acfg_dmname = []
+    self.acfg_stat_cols = []
+    self.acfg_stat_name = []
+    self.acfg_threshold = []
 
+class auto_cfg(object):
+  def __init__(self, section, ksec):
+    self.acfg = section
+    self.alen = len(section)
+    self.acfg_name = []
+    self.acfg_stat_name= []
+    self.acfg_stat_cols= []
+    self.acfg_threshold = []
+    self.acfg_kidx = []
+    self.acfg_key = []
+
+    for icfg, acfg_item in enumerate(self.acfg):
+      #cfg-name
+      print self.acfg[acfg_item]
+      keylist, value, threshold = parse_agpstr(self.acfg[acfg_item])
+      self.acfg_name.append(acfg_item)
+      self.acfg_stat_name.append([])
+      self.acfg_stat_cols.append([])
+      self.acfg_threshold.append([])
+
+      for v, t in zip(value, threshold):
+        if v in ksec.keys():
+          self.acfg_stat_name[icfg].append(v)
+          self.acfg_stat_cols[icfg].append(ksec[v])
+          self.acfg_threshold[icfg].append(int(t))
+        else:
+          raise ValueError("Config do not have a right format!", str(self.acfg[acfg_item]))
+
+      self.acfg_kidx.append([])
+      self.acfg_key.append([])
+
+      for ikey, key in enumerate(keylist):
+        try:
+          #print ikey, key
+          self.acfg_key[icfg].append(key)
+          self.acfg_kidx[icfg].append(int(ksec[key]))
+          pass
+        except KeyError:
+          raise ValueError("%r, Invalid config key! %r", key, ksec.keys())
+
+      print "acfg:", icfg, self.acfg_name, self.acfg_key[icfg], self.acfg_kidx[icfg], \
+          self.acfg_stat_name[icfg], self.acfg_stat_cols[icfg], self.acfg_threshold[icfg]
+
+  def get_acfg_num(self):
+    return self.alen
+
+  def get_acfg_name(self, aidx):
+    if adix >= self.alen:
+      return None
+    return self.acfg_name[aidx]
+
+  def get_acfg_dmlist(self, aidx):
+    if adix >= self.alen:
+      return []
+    return self.acfg_kidx[aidx]
+  
+  def get_acfg_dmname(self, aidx):
+    if adix >= self.alen:
+      return []
+    return self.acfg_key[aidx]
+  
+  def get_acfg_stat_cols(self, aidx):
+    if adix >= self.alen:
+      return []
+    return self.acfg_stat_cols[aidx]
+  
+  def get_acfg_stat_name(self, aidx):
+    if adix >= self.alen:
+      return []
+    return self.acfg_stat_name[aidx]
+  
+  def get_acfg_threshold(self, aidx):
+    if adix >= self.alen:
+      return []
+    return self.acfg_threshold[aidx]
+
+  def get_acfg_item(self, aidx, acfg_item):
+    if adix >= self.alen:
+      return
+
+    acfg_item.acfg_name = self.get_acfg_name(adix)
+    acfg_item.acfg_dmlist = self.get_acfg_dmlist(adix)
+    acfg_item.acfg_dmname = self.get_acfg_dmname(adix)
+    acfg_item.acfg_stat_cols = self.get_acfg_stat_cols(adix)
+    acfg_item.acfg_stat_name = self.get_acfg_stat_name(adix)
+    acfg_item.acfg_threshold = self.get_acfg_threshold(adix)
+
+    return
+    
 class spc_gcfg(object):
   def __init__(self, section, ksec):
     self.scfg = section
@@ -133,6 +186,7 @@ class spc_gcfg(object):
     self.scfg_keyidx = []
     self.scfg_keyname = []
     self.scfg_keymatch = []
+    self.scfg_matchidx = []
     self.scfg_agp = []
 
     for iscfg , scfg_item in enumerate(self.scfg):
@@ -140,6 +194,7 @@ class spc_gcfg(object):
       self.scfg_keyidx.append([])
       self.scfg_keyname.append([])
       self.scfg_keymatch.append([])
+      self.scfg_matchidx.append([])
 
       self.scfg_agp.append([])
       #first elem save group number
@@ -147,7 +202,7 @@ class spc_gcfg(object):
 
       print self.scfg[scfg_item]
       for iagp, agp_item in enumerate(self.scfg[scfg_item]):
-        print iagp, agp_item, self.scfg[scfg_item][agp_item]
+        #print iagp, agp_item, self.scfg[scfg_item][agp_item]
         if agp_item in ksec.keys():
           try:
             self.scfg_keyname[iscfg].append(agp_item)
@@ -155,25 +210,35 @@ class spc_gcfg(object):
             self.scfg_keymatch[iscfg].append(self.scfg[scfg_item][agp_item])
           except:
             pass
-          print '---scfg---', iscfg, self.scfg_keyname[iscfg], \
-            self.scfg_keyidx[iscfg], self.scfg_keymatch[iscfg]
         else:
           #auto group item
           self.scfg_agp[iscfg][0] += 1
           a_idx = self.scfg_agp[iscfg][0]
           
-          keylist, value, threshold = parse_agpstr(self.scfg[scfg_item][agp_item])
-          print '----what---', keylist
+          keylist, colname_list, thrd_list = parse_agpstr(self.scfg[scfg_item][agp_item])
+          if len(colname_list) != len(thrd_list):
+            raise ValueError("Config do not have a right format!", \
+                str(self.scfg[scfg_item][agp_item]))
 
           self.scfg_agp[iscfg].append([])
           #save agp cfg: name, value_name, value_col, threshold
           self.scfg_agp[iscfg][a_idx].append(agp_item)
-          if value in ksec.keys():
-            self.scfg_agp[iscfg][a_idx].append(value)
-            self.scfg_agp[iscfg][a_idx].append(ksec[value])
-            self.scfg_agp[iscfg][a_idx].append(int(threshold))
-          else:
-            raise ValueError("Config do not have a right format!", str(self.scfg[scfg_item][agp_item]))
+          #stat col name
+          self.scfg_agp[iscfg][a_idx].append([])
+          #stat col 
+          self.scfg_agp[iscfg][a_idx].append([])
+          #stat threshold
+          self.scfg_agp[iscfg][a_idx].append([])
+          
+          for v, t in zip(colname_list, thrd_list):
+            if v in ksec.keys():
+              self.scfg_agp[iscfg][a_idx][1].append(v)
+              self.scfg_agp[iscfg][a_idx].append([])
+              self.scfg_agp[iscfg][a_idx][2].append(int(ksec[v]))
+              self.scfg_agp[iscfg][a_idx].append([])
+              self.scfg_agp[iscfg][a_idx][3].append(int(t))
+            else:
+              raise ValueError("Config do not have a right format!", str(self.scfg[scfg_item][agp_item]))
 
           #keyidx list and keyname list
           self.scfg_agp[iscfg][a_idx].append([])
@@ -183,13 +248,34 @@ class spc_gcfg(object):
           # i don't know how to use maroc now, so 4/5...
           for ikey, key in enumerate(list(keylist)):
             try:
-              print '--', ikey, key
               self.scfg_agp[iscfg][a_idx][4].append(int(ksec[key]))
               self.scfg_agp[iscfg][a_idx][5].append(key)
             except:
               pass
             
-          print '---', iscfg, a_idx, self.scfg_agp[iscfg][a_idx]
+          print 'scfgapc:', iscfg, a_idx, self.scfg_agp[iscfg][a_idx]          
+        
+      print 'scfg:', iscfg, self.scfg_keyname[iscfg], \
+            self.scfg_keyidx[iscfg], self.scfg_keymatch[iscfg]
+
+  def chk_spcagc_cfg(self, sidx):
+    # check agpconfig item, agpkeyname should not contain any word in spcfg
+    spc_dmlist = self.get_spc_dmlist(sidx)
+    if len(spc_dmlist) < 1:
+      return
+    for a_idx in range(self.get_spcagc_num(sidx)):
+      spcagc_dmlist = self.get_spcagc_dmlist(iscfg, a_idx - 1)
+      spcagc_dmname = self.get_spcagc_dmname(iscfg, a_idx - 1)
+      if len(spcagc_dmname) != len(spcagc_dmlist):
+        raise ValueError("Config Error:", spcagc_dmname, spcagc_dmlist)
+      for idx, akeyidx in enumerate(spcagc_dmlist):
+        if akeyidx in self.get_spc_dmlist(sidx):
+          del spcagc_dmlist[idx]
+          del spcagc_dmname[idx]
+          #delete and warning
+          print "Agpcfg domain is dupplicated spcfg:", spcagc_dmname, self.get_spc_dmname(sidx)
+      pass
+    pass
 
   # get spcfg number
   def get_spcfg_num(self):
@@ -205,6 +291,20 @@ class spc_gcfg(object):
     if sidx >= self.slen:
       return []    
     return self.scfg_keyidx[sidx]
+
+  def get_spc_matchlist(self, sidx):
+    if sidx >= self.slen:
+      return []    
+    return self.scfg_matchidx[sidx]
+
+  def set_spc_matchlist(self, sidx, idxlist):
+    if sidx >= self.slen:
+      return []
+    if len(idxlist) != len(self.scfg_keyidx[sidx]):
+      print "Config convert Error:", self.scfg_keyname[sidx], self.scfg_keyidx[sidx], idxlist
+    self.scfg_matchidx[sidx] = copy.deepcopy(idxlist)
+    return
+
   def get_spc_dmmatch(self, sidx):
     if sidx >= self.slen:
       return []    
@@ -221,35 +321,46 @@ class spc_gcfg(object):
     return self.scfg_agp[sidx][0]
 
   def get_spcagc_name(self, sidx, aidx):
-    if sidx >= self.slen or aidxi + 1 >= self.get_spcagc_num(sidx):
+    if sidx >= self.slen or aidx + 1 >= self.get_spcagc_num(sidx):
       return None
     return self.scfg_agp[sidx][aidx + 1][0]
  
   def get_spcagc_dmlist(self, sidx, aidx):
-    if sidx >= self.slen or aidxi + 1 >= self.get_spcagc_num(sidx):
+    if sidx >= self.slen or aidx + 1 > self.get_spcagc_num(sidx):
       return []
     return self.scfg_agp[sidx][aidx + 1][4]
     
   def get_spcagc_dmname(self, sidx, aidx):
-    if sidx >= self.slen or aidxi + 1 >= self.get_spcagc_num(sidx):
+    if sidx >= self.slen or aidx + 1 > self.get_spcagc_num(sidx):
       return []
     return self.scfg_agp[sidx][aidx + 1][5]
   
   def get_spcagc_statcol(self, sidx, aidx):
-    if sidx >= self.slen or aidxi + 1 >= self.get_spcagc_num(sidx):
+    if sidx >= self.slen or aidx + 1 > self.get_spcagc_num(sidx):
       return []
     return self.scfg_agp[sidx][aidx + 1][2]
 
-  def get_spcagc_statcol_name(self, sidx, aidx):
-    if sidx >= self.slen or aidxi + 1 >= self.get_spcagc_num(sidx):
+  def get_spcagc_stat_name(self, sidx, aidx):
+    if sidx >= self.slen or aidx + 1 > self.get_spcagc_num(sidx):
       return []
     return self.scfg_agp[sidx][aidx + 1][1]
 
   def get_spcagc_threshold(self, sidx, aidx):
-    if sidx >= self.slen or aidxi + 1 >= self.get_spcagc_num(sidx):
+    if sidx >= self.slen or aidx + 1 > self.get_spcagc_num(sidx):
       return []
     return self.scfg_agp[sidx][aidx + 1][3]
+  
+  def get_spcagc_item(self, sidx, aidx, acfg_item):
+    if sidx >= self.slen or aidx + 1 > self.get_spcagc_num(sidx):
+      return False
+    acfg_item.acfg_name = self.get_spcagc_name(sidx, adix)
+    acfg_item.acfg_dmlist = self.get_spcagc_dmlist(sidx, adix)
+    acfg_item.acfg_dmname = self.get_spcagc_dmname(sidx, adix)
+    acfg_item.acfg_stat_cols = self.get_spcagc_stat_cols(sidx, adix)
+    acfg_item.acfg_stat_name = self.get_spcagc_stat_name(sidx, adix)
+    acfg_item.acfg_threshold = self.get_spcagc_threshold(sidx, adix)
 
+    return True
 
 def getcfg(filename):
   return config
@@ -262,9 +373,8 @@ def parse_cfg(filename):
   return sec_path, sec_opt, sec_agp, sec_sgp
 
 if __name__ == "__main__":
-  sec_opt, sec_agp, sec_sgp = parse_cfg('./config.txt')
+  sec_path, sec_opt, sec_agp, sec_sgp = parse_cfg('./config.txt')
   spc = spc_gcfg(sec_sgp, sec_opt)
-  t=threading.Timer(2, tm_action, [pline])
-  t.start()
-
+  print '================='
+  agc = auto_cfg(sec_agp, sec_opt)
 
